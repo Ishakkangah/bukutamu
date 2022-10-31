3b1abf077190ba8688bc03f030619e0f2d3a1a2a
ishakk angah <ishakkangah@gmail.com>
2022-10-31 10:34:56 +0700

    menginstal aplikasi laravel dari 0

---
+++ app/Http/Controllers/BukutamuController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\bukutamu;
+use App\Http\Requests\StorebukutamuRequest;
+use App\Http\Requests\UpdatebukutamuRequest;
+
+class BukutamuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorebukutamuRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorebukutamuRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\bukutamu  $bukutamu
+     * @return \Illuminate\Http\Response
+     */
+    public function show(bukutamu $bukutamu)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\bukutamu  $bukutamu
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(bukutamu $bukutamu)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatebukutamuRequest  $request
+     * @param  \App\Models\bukutamu  $bukutamu
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatebukutamuRequest $request, bukutamu $bukutamu)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\bukutamu  $bukutamu
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(bukutamu $bukutamu)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_10_31_032754_create_bukutamus_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBukutamusTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('bukutamus', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('instansi');
+            $table->date('date');
+            $table->text('perihal');
+            $table->string('tujuan');
+            $table->string('keterangan')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('bukutamus');
+    }
+}
