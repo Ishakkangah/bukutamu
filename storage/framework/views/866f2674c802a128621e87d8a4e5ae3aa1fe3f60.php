da70f441190eb866a64814597576391df19a2d09
ishakk angah <ishakkangah@gmail.com>
2023-02-15 09:42:01 +0700

    solve

--- storage/framework/views/866f2674c802a128621e87d8a4e5ae3aa1fe3f60.php
+++ storage/framework/views/866f2674c802a128621e87d8a4e5ae3aa1fe3f60.php
@@ -198,7 +198,8 @@
                                 <?php echo method_field('patch'); ?>
                                 <?php echo csrf_field(); ?>
                                 <!-- Buka kamera -->
-                                <input data-modal-target="defaultModal" data-modal-toggle="defaultModal"
+                                <input type="button" data-modal-target="defaultModal"
+                                    data-modal-toggle="defaultModal"
                                     class="block text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 w-full sm:w-auto bukaKamera tampilKamera cursor-pointer"
                                     value="BUKA KAMERA" onClick="buka_kamera()" data-toggle="modal"
                                     data-target="#cameraModal">
